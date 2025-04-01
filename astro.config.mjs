0bef77d588da591d742cc8ad2c3381d7f92e1ac9
sn0wzZz <sn0wzzz.dev@gmail.com>
2025-04-01 17:57:45 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    integrations: [mdx(), sitemap(), react()],
-    output: 'static',
+  site: 'https://example.com',
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [mdx(), sitemap(), react()],
+  output: 'static',
+  adapter: vercel(),
 });
